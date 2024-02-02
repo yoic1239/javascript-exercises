16cf99da4533f37d295fcc2f83cc18ec44ce920f
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2024-02-02 12:51:22 -0700

    Complete 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reverseWord = '';
+    for (let i = word.length - 1; i >= 0; i--) {
+        reverseWord += word[i];
+    }
+    return reverseWord
 };
 
 // Do not edit below this line
