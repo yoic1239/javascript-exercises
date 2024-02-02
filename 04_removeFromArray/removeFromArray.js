748c3d151c87087ad413102e18715ee06ecc4abb
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2024-02-02 14:09:00 -0700

    Complete 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    let removed = [];
+    outer: for (let i = 0; i < array.length; i++) {
+        for (let j = 1; j < arguments.length; j++) {
+            if (array[i] === arguments[j]) continue outer;
+        }
+        removed.push(array[i]);
+    }
+    return removed;
 };
 
 // Do not edit below this line
