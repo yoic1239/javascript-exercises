4310d6e5960de69ac88bb4e13b53844e811f0a92
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2024-02-02 12:40:29 -0700

    Complete 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (times < 0) return 'ERROR';
+    
+    let repeated = '';
+    for (let i = 0; i < times; i++) {
+        repeated += string;
+    }
+    return repeated;
 };
 
 // Do not edit below this line
