478d0aa729f280b337eaab26aa2ee1d7bbfa7ae3
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2024-02-05 10:17:59 -0700

    Complete 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let sorted = people.sort(function(person1, person2) {
+        const age1 = (("yearOfDeath" in person1) ? person1.yearOfDeath : new Date().getFullYear()) - person1.yearOfBirth;
+        const age2 = (("yearOfDeath" in person2) ? person2.yearOfDeath : new Date().getFullYear()) - person2.yearOfBirth;
+        return age2 - age1;
+    });
+    return sorted.find(person => person.name === sorted[0].name);
 };
 
 // Do not edit below this line
