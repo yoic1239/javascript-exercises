10c41266c903d614f809cebad26fd08f536c7765
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2024-02-05 00:08:35 -0700

    Complete 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
 
+    if (num < 0) {
+        return 'OOPS';
+    }
+    
+    let seq = [0, 1];
+    for (let i = 2; i <= num; i++) {
+        seq.push(seq[i-2] + seq[i-1]);
+    }
+    return seq[num];
 };
 
 // Do not edit below this line
