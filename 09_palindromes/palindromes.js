3a4bd9826ca3114455b085acf9caed3aa3ea0325
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2024-02-04 23:50:56 -0700

    Complete 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    let processed = words.toLowerCase().replace(/[^a-z0-9]/g, '');
+    return processed.split('').reverse().join('') == processed;
 };
 
 // Do not edit below this line
